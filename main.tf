4466a1f49c118ba4c872e209e91469e0ee647f39
Avan patel <avanpatel01133@gmail.com>
2024-11-11 17:54:27 +0530

    final code

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my-bucket" {
   name          = var.bucket_name
-  region      = var.region
+  location = "US"
   project = var.project
   force_destroy = true
   public_access_prevention = "enforced"
