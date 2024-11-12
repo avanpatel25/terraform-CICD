54b4072f467c3f1ca624855600d24940721dfbd6
Avan patel <avanpatel01133@gmail.com>
2024-11-12 22:32:13 +0530

    env/dev folder added

---
+++ env/dev/variable.tf
@@ -0,0 +1,15 @@
+
+variable "region" {
+  type = string
+  description = "Location of the Google Storage bucket"
+}
+
+variable "project" {
+  type = string
+  description = "Project ID of the Google Cloud Platform project"
+}
+
+variable "zone" {
+  type = string
+  description = "Location of the Google Storage bucket creared with master"
+}
