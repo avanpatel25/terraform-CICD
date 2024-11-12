f44cb56c5435341b7cabd26937af5acbc01d9b11
avanpatel25 <avanpatel25@gmail.com>
2024-11-12 09:09:50 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,17 +2,14 @@
 variable "region" {
   type = string
   description = "Location of the Google Storage bucket"
-  default = "asia-south1"
 }
 
 variable "project" {
   type = string
   description = "Project ID of the Google Cloud Platform project"
-  default = "cicd0626"
 }
 
 variable "zone" {
   type = string
   description = "Location of the Google Storage bucket"
-  default = "asia-south1-a"
 }
